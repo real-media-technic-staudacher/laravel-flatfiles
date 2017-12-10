bf6cd099b05282dbaa959e1dc7404b86a5321019
Patrick Thomas <github@patrick-thomas.de>
2017-12-10 17:51:36 +0100

    Added orchestra

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,8 +6,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    use CreatesApplication;
-
     protected function getPackageProviders($app)
     {
         return ['LaravelFlatfiles\Providers\LaravelFlatfilesServiceProvider'];
