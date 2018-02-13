18ff208648358aae3af61e8156f1aeac2b8b8a01
patriziotomato <github@patrick-thomas.de>
2018-02-13 16:35:06 +0000

    Apply fixes from StyleCI

--- src/FlatfileExport.php
+++ src/FlatfileExport.php
@@ -2,13 +2,13 @@
 
 namespace LaravelFlatfiles;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Filesystem\FilesystemAdapter;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 use League\Csv\Writer;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class FlatfileExport
 {
@@ -87,14 +87,14 @@ class FlatfileExport
 
         if ($replace && $fileExists) {
             \Log::debug('Delete existing export file: '.$absoluteFilepath);
-            $fileExists = !unlink($absoluteFilepath);
+            $fileExists = ! unlink($absoluteFilepath);
         }
 
         if ($fileExists) {
             throw new \RuntimeException('Target export file already exists at: '.$absoluteFilepath);
         }
 
-        if (!file_exists(dirname($absoluteFilepath))) {
+        if (! file_exists(dirname($absoluteFilepath))) {
             mkdir($absoluteFilepath, 0777, true);
         }
 
@@ -177,7 +177,7 @@ class FlatfileExport
     {
         $this->addBomIfNeeded();
 
-        if (!$this->usesDisk()) {
+        if (! $this->usesDisk()) {
             if ($this->pathToLocalTmpFile == $this->pathToFile()) {
                 return true;
             }
@@ -200,7 +200,7 @@ class FlatfileExport
 
         switch ($extension = $this->targetfileExtension()) {
             case 'csv':
-                if (!$this->pathToLocalTmpFile) {
+                if (! $this->pathToLocalTmpFile) {
                     if ($this->usesDisk()) {
                         $this->pathToLocalTmpFile = tempnam(sys_get_temp_dir(), 'ffe');
                     } else {
@@ -268,7 +268,7 @@ class FlatfileExport
      */
     private function makeModelAttributesVisible($model)
     {
-        if (!($model instanceof Model)) {
+        if (! ($model instanceof Model)) {
             return $model;
         }
 
@@ -282,7 +282,7 @@ class FlatfileExport
 
     private function addBomIfNeeded()
     {
-        if ($this->bomNeedsToBeAdded && !$this->checkbom()) {
+        if ($this->bomNeedsToBeAdded && ! $this->checkbom()) {
             file_put_contents($this->pathToLocalTmpFile, Writer::BOM_UTF8.file_get_contents($this->pathToLocalTmpFile));
             $this->bomNeedsToBeAdded = false;
         }
@@ -298,7 +298,7 @@ class FlatfileExport
 
     private function toArrayWithoutSnakeCasedKeys($model)
     {
-        if (!($model instanceof Model)) {
+        if (! ($model instanceof Model)) {
             return $model->toArray();
         }
 
